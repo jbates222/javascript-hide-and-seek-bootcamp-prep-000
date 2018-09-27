cff01682b7307c84b17c0068ee3df0733aca7ad0
Jackson Bates <jkbates222@gmail.com>
2018-09-27 00:37:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.querySelector('#nested').querySelector('.target');
+}
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+      const firstList = rankedLists[0];
+      const secondList = rankedLists[1];
 
+      let children = firstList.children;
+      let start = 1;
+      for (let i = 0, l = children.length; i < l; i++) {
+        children[i].innerHTML = (start + i + 3);
+      }
+
+      children = secondList.children;
+      start = 12;
+      for (let i = 0, l = children.length; i < l; i++) {
+        children[i].innerHTML = (start - i + 3);
+      }
+}
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
